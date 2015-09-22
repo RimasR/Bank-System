51dc05a83ac851db35794b8022fd9ebb48cd0af4
Kristupas Savickas <savickas.kristupas@gmail.com>
2015-09-22 19:08:03 +0300

    make BankAccount class file

---
+++ BankSystem/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿namespace BankSystem
+{
+    class BankAccount
+    {
+        public PermissionTypes permissions = PermissionTypes.None;
+        public string name
+        { get; set; }
+        public string surname
+        { get; set; }
+        public int id;
+        public int pass;
+        public string year;
+        double money
+        { get; set; }
+        public BankAccount()
+        {
+
+        }
+        public BankAccount(string name, string surname, string year)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.year = year;
+        }
+    }
+}
--- BankSystem/Program.cs
+++ BankSystem/Program.cs
@@ -106,29 +106,6 @@ namespace BankSystem
         }
     }
 
-    class BankAccount
-    {
-        public PermissionTypes permissions = PermissionTypes.None;
-        public string name
-        { get; set; }
-        public string surname
-        { get; set; }
-        public int id;
-        public int pass;
-        public string year;
-        double money
-        { get; set; }
-        public BankAccount()
-        {
-
-        }
-        public BankAccount(string name, string surname, string year)
-        {
-            this.name = name;
-            this.surname = surname;
-            this.year = year;
-        }
-    }
 }
 
 //Sukurti registration forma +
